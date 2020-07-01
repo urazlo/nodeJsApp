16bcf9c13a137adba1e1daa8346d47e2efee7e70
urazlo <assaurasins@gmail.com>
2020-07-01 14:53:45 +0300

    Removed isAdmin route

--- middlewhares/isAdmin.js
+++ middlewhares/isAdmin.js
@@ -1,12 +1,9 @@
 module.exports = async (req, res, next) => {
   try {
-    
-    console.log(req.user.role);
     if (req.user.role !== 'admin') {
-      console.log('Permission denied');
       return res.sendStatus(403);
     }
-    console.log('Ok');
+
     next();
   }
   catch (err) {
